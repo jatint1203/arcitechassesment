6917ba27dfaf1bbd4cde56ee9c1650a50506d641
jatintwork <jatintiwari9312@gmail.com>
2023-11-14 14:46:56 +0530

    Removing SQL DB

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from blog.models import Comment, Post
+# from blog.models import Comment, Post
 
 # Register your models here.
 
-admin.site.register(Comment)
-admin.site.register(Post)
+# admin.site.register(Comment)
+# admin.site.register(Post)
--- blog/models.py
+++ blog/models.py
@@ -3,22 +3,22 @@ from django.db import models
 # Create your models here.
 
 
-class Post(models.Model):
-    id = models.AutoField(primary_key=True)
-    title = models.CharField(max_length=100)
-    content = models.TextField()
-    author = models.CharField(max_length=10)
-    created_at = models.DateField()
+# class Post(models.Model):
+#     id = models.AutoField(primary_key=True)
+#     title = models.CharField(max_length=100)
+#     content = models.TextField()
+#     author = models.CharField(max_length=10)
+#     created_at = models.DateField()
 
-    def __str__(self):
-        return self.title
+#     def __str__(self):
+#         return self.title
     
 
-class Comment(models.Model):
-    author = models.AutoField(primary_key=True)
-    content = models.CharField(max_length=100)
-    created_at = models.DateField()
+# class Comment(models.Model):
+#     author = models.AutoField(primary_key=True)
+#     content = models.CharField(max_length=100)
+#     created_at = models.DateField()
   
 
-    def __str__(self):
-        return self.author
+#     def __str__(self):
+#         return self.author
